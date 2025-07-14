a037801e08497f00279ca97198e303c770294643
Zied-BenBahri <ziedbenbahri@gmail.com>
2025-07-14 10:37:15 +0100

    updating output (Visualizing public and private IPs)

--- outputs.tf
+++ outputs.tf
@@ -45,36 +45,39 @@ output "gateway_subnet" {
   }
 }
 
-# Public IP
-output "proxy_public_ip" {
-  value = azurerm_public_ip.proxy_ip.ip_address
-}
 # Virtual Machines
 output "proxy_vm" {
   value = {
-    name     = azurerm_windows_virtual_machine.proxy_vm.name
-    location = azurerm_windows_virtual_machine.proxy_vm.location
-    size     = azurerm_windows_virtual_machine.proxy_vm.size
+    name       = azurerm_windows_virtual_machine.proxy_vm.name
+    location   = azurerm_windows_virtual_machine.proxy_vm.location
+    size       = azurerm_windows_virtual_machine.proxy_vm.size
+    ip_address = azurerm_public_ip.proxy_ip.ip_address
+    private_ip = azurerm_network_interface.proxy_nic.ip_configuration[0].private_ip_address
   }
 }
 output "monitor_vm" {
   value = {
-    name     = azurerm_linux_virtual_machine.monitor_vm.name
-    location = azurerm_linux_virtual_machine.monitor_vm.location
-    size     = azurerm_linux_virtual_machine.monitor_vm.size
+    name       = azurerm_linux_virtual_machine.monitor_vm.name
+    location   = azurerm_linux_virtual_machine.monitor_vm.location
+    size       = azurerm_linux_virtual_machine.monitor_vm.size
+    private_ip = azurerm_network_interface.monitor_nic.ip_configuration[0].private_ip_address
   }
 }
 output "vm-iis-01" {
   value = {
-    name     = azurerm_windows_virtual_machine.vm_iis_01.name
-    location = azurerm_windows_virtual_machine.vm_iis_01.location
-    size     = azurerm_windows_virtual_machine.vm_iis_01.size
+    name       = azurerm_windows_virtual_machine.vm_iis_01.name
+    location   = azurerm_windows_virtual_machine.vm_iis_01.location
+    size       = azurerm_windows_virtual_machine.vm_iis_01.size
+    ip_address = azurerm_public_ip.iis01_ip.ip_address
+    private_ip = azurerm_network_interface.vm_iis_01_nic.ip_configuration[0].private_ip_address
   }
 }
 output "vm-iis-02" {
   value = {
-    name     = azurerm_windows_virtual_machine.vm_iis_02.name
-    location = azurerm_windows_virtual_machine.vm_iis_02.location
-    size     = azurerm_windows_virtual_machine.vm_iis_02.size
+    name       = azurerm_windows_virtual_machine.vm_iis_02.name
+    location   = azurerm_windows_virtual_machine.vm_iis_02.location
+    size       = azurerm_windows_virtual_machine.vm_iis_02.size
+    ip_address = azurerm_public_ip.iis02_ip.ip_address
+    private_ip = azurerm_network_interface.vm_iis_02_nic.ip_configuration[0].private_ip_address
   }
 }
