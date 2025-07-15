255ae20f3d853c3e7633b67b67187e14c7a65f1e
Zied-BenBahri <ziedbenbahri@gmail.com>
2025-07-15 21:58:25 +0100

    Add private IP outputs for virtual machines and install WMI Exporter in setup script

--- outputs.tf
+++ outputs.tf
@@ -52,28 +52,35 @@ output "proxy_vm" {
     location   = azurerm_windows_virtual_machine.proxy_vm.location
     size       = azurerm_windows_virtual_machine.proxy_vm.size
     ip_address = azurerm_public_ip.proxy_ip.ip_address
+    private_ip = azurerm_network_interface.proxy_nic.private_ip_address
   }
 }
+
 output "monitor_vm" {
   value = {
-    name     = azurerm_linux_virtual_machine.monitor_vm.name
-    location = azurerm_linux_virtual_machine.monitor_vm.location
-    size     = azurerm_linux_virtual_machine.monitor_vm.size
+    name       = azurerm_linux_virtual_machine.monitor_vm.name
+    location   = azurerm_linux_virtual_machine.monitor_vm.location
+    size       = azurerm_linux_virtual_machine.monitor_vm.size
+    private_ip = azurerm_network_interface.monitor_nic.private_ip_address
   }
 }
+
 output "vm-iis-01" {
   value = {
     name       = azurerm_windows_virtual_machine.vm_iis_01.name
     location   = azurerm_windows_virtual_machine.vm_iis_01.location
     size       = azurerm_windows_virtual_machine.vm_iis_01.size
     ip_address = azurerm_public_ip.iis01_ip.ip_address
+    private_ip = azurerm_network_interface.iis01_nic.private_ip_address
   }
 }
+
 output "vm-iis-02" {
   value = {
     name       = azurerm_windows_virtual_machine.vm_iis_02.name
     location   = azurerm_windows_virtual_machine.vm_iis_02.location
     size       = azurerm_windows_virtual_machine.vm_iis_02.size
     ip_address = azurerm_public_ip.iis02_ip.ip_address
+    private_ip = azurerm_network_interface.iis02_nic.private_ip_address
   }
 }
--- scripts/setup-proxy.ps1
+++ scripts/setup-proxy.ps1
@@ -3,6 +3,14 @@
 # Install IIS
 Install-WindowsFeature -Name Web-Server, Web-WebSockets, Web-Mgmt-Tools -IncludeAllSubFeature
 
+# Download and install WMI Exporter
+Invoke-WebRequest -Uri "https://github.com/prometheus-community/windows_exporter/releases/download/v0.24.0/windows_exporter-0.24.0-amd64.msi" -OutFile "C:\windows_exporter.msi"
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\windows_exporter.msi /quiet'
+
+# Enable and start the service
+Start-Service windows_exporter
+Set-Service windows_exporter -StartupType Automatic
+
 # Install WebPI
 Invoke-WebRequest -Uri "https://aka.ms/webpicmd" -OutFile "WebPI.msi"
 Start-Process msiexec.exe -Wait -ArgumentList '/I WebPI.msi /quiet'
