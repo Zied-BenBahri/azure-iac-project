331c6a61ed6442595557a961960a4fa200248355
Zied-BenBahri <ziedbenbahri@gmail.com>
2025-07-23 12:20:21 +0100

    Add VPN gateway configuration to Terraform script

---
+++ vpn.tf
@@ -0,0 +1,25 @@
+# This creates the gateway that remote clients (like the on-prem server) will connect to.
+resource "azurerm_public_ip" "vpn_gateway_ip" {
+  name                = "vpn-gateway-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+resource "azurerm_virtual_network_gateway" "vpn_gateway" {
+  name                = "vnet-gateway"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  type                = "Vpn"
+  vpn_type            = "RouteBased"
+  active_active       = false
+  enable_bgp          = false
+  sku                 = "VpnGw1"
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.vpn_gateway_ip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.gateway_subnet.id
+  }
+}
