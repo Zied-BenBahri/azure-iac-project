8a10f5ed7312d22289174276dbbdcbc47a0c200a
Zied-BenBahri <ziedbenbahri@gmail.com>
2025-07-23 11:20:26 +0100

    x

--- scripts/setup-iis.ps1
+++ scripts/setup-iis.ps1
@@ -20,7 +20,7 @@ Write-Host "Installing WMI Exporter..."
 Invoke-WebRequest -Uri "https://github.com/prometheus-community/windows_exporter/releases/download/v0.24.0/windows_exporter-0.24.0-amd64.msi" -OutFile "C:\windows_exporter.msi"
 Start-Process msiexec.exe -Wait -ArgumentList '/I C:\windows_exporter.msi /quiet'
 
-# Enable and start the service
+# Enable and start the service 
 Start-Service windows_exporter
 Set-Service windows_exporter -StartupType Automatic
 
