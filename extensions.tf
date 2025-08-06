4684fda2dbe672522591fe7aaebb840ab0313673
Zied-BenBahri <ziedbenbahri@gmail.com>
2025-08-07 00:40:50 +0100

    Default config without custom script

--- extensions.tf
+++ extensions.tf
@@ -1,25 +1,6 @@
-resource "azurerm_virtual_machine_extension" "iis01_setup" {
-  name                 = "iis-setup"
-  virtual_machine_id   = azurerm_windows_virtual_machine.vm_iis_01.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.10"
 
-  settings = jsonencode({
-    "fileUris" = [
-      "https://raw.githubusercontent.com/Zied-BenBahri/azure-iac-project/main/scripts/setup-iis-optimized.ps1"
-    ],
-  "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File setup-iis-optimized.ps1" })
 
-  timeouts {
-    create = "60m"
-    update = "60m"
-    delete = "10m"
-  }
-
-  depends_on = [azurerm_windows_virtual_machine.vm_iis_01]
-}
-
+/*
 resource "azurerm_virtual_machine_extension" "iis02_setup" {
   name                 = "iis-setup"
   virtual_machine_id   = azurerm_windows_virtual_machine.vm_iis_02.id
@@ -41,7 +22,7 @@ resource "azurerm_virtual_machine_extension" "iis02_setup" {
 
   depends_on = [azurerm_windows_virtual_machine.vm_iis_02]
 }
-
+*/
 resource "azurerm_virtual_machine_extension" "monitor_script" {
   name                 = "setup-monitoring"
   virtual_machine_id   = azurerm_linux_virtual_machine.monitor_vm.id
--- vms.tf
+++ vms.tf
@@ -1,4 +1,4 @@
-# Monitoring NIC
+# Monitoring resources (keep unchanged)
 resource "azurerm_network_interface" "monitor_nic" {
   name                = "nic-monitor"
   location            = var.location
@@ -34,7 +34,8 @@ resource "azurerm_linux_virtual_machine" "monitor_vm" {
     version   = "latest"
   }
 }
-# NIC for vm-iis-01
+
+# NICs for IIS VMs (keep unchanged)
 resource "azurerm_network_interface" "iis01_nic" {
   name                = "nic-iis01"
   location            = var.location
@@ -44,11 +45,9 @@ resource "azurerm_network_interface" "iis01_nic" {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.backend.id
     private_ip_address_allocation = "Dynamic"
-    #public_ip_address_id          = azurerm_public_ip.iis01_ip.id
   }
 }
 
-# NIC for vm-iis-02
 resource "azurerm_network_interface" "iis02_nic" {
   name                = "nic-iis02"
   location            = var.location
@@ -58,10 +57,16 @@ resource "azurerm_network_interface" "iis02_nic" {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.backend.id
     private_ip_address_allocation = "Dynamic"
-    #public_ip_address_id          = azurerm_public_ip.iis02_ip.id
   }
 }
-# VM: vm-iis-01
+
+# Data source to reference the existing custom image
+data "azurerm_image" "vm_iis_image" {
+  name                = "vm-iis-blazor-image"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# VM: vm-iis-01 (Recreated from custom image)
 resource "azurerm_windows_virtual_machine" "vm_iis_01" {
   name                  = "vm-iis-01"
   location              = var.location
@@ -71,6 +76,9 @@ resource "azurerm_windows_virtual_machine" "vm_iis_01" {
   admin_password        = var.admin_password
   network_interface_ids = [azurerm_network_interface.iis01_nic.id]
 
+  # Use custom image with pre-installed Blazor app
+  # source_image_id = data.azurerm_image.vm_iis_image.id
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -82,21 +90,40 @@ resource "azurerm_windows_virtual_machine" "vm_iis_01" {
     sku       = "2022-Datacenter"
     version   = "latest"
   }
+
+  tags = {
+    Environment = "Production"
+    Role        = "WebServer"
+    Application = "BlazorApp"
+  }
+
+  # depends_on = [data.azurerm_image.vm_iis_image]
 }
 
-# VM: vm-iis-02
+# VM: vm-iis-02 (Created from custom image)
 resource "azurerm_windows_virtual_machine" "vm_iis_02" {
-  name                  = "vm-iis-02"
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.main.name
-  size                  = "Standard_B2ms"
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  network_interface_ids = [azurerm_network_interface.iis02_nic.id]
+  name                = "vm-iis-02"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B2ms"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.iis02_nic.id,
+  ]
+
+  # Use custom image with pre-installed Blazor app
+  # source_image_id = data.azurerm_image.vm_iis_image.id
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
+  }
+
+  tags = {
+    Environment = "Production"
+    Role        = "WebServer"
+    Application = "BlazorApp"
   }
 
   source_image_reference {
@@ -105,4 +132,7 @@ resource "azurerm_windows_virtual_machine" "vm_iis_02" {
     sku       = "2022-Datacenter"
     version   = "latest"
   }
+
+  # depends_on = [data.azurerm_image.vm_iis_image]
 }
+
